e420186759053ef80868a7230d4e272a4b37d5f9
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-02-28 11:40:26 +0100

    add admin show action & view

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,5 +1,16 @@
 class Admin::BookingsController < ApplicationController
+  before_action :set_bookings, only: %i[show]
+
   def index
     @bookings = Booking.joins(:artist).where(artist: { user: current_user })
+  end
+
+  def show
+  end
+
+  private
+
+  def set_bookings
+    @booking = Booking.find(params[:id])
   end
 end
