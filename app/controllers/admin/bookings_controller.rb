c377f4ccb7a6f05936a3d2b9dbaaf9bb50c65422
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-02-28 11:22:01 +0100

    add admin index controller & view

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,5 +1,5 @@
 class Admin::BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = Booking.joins(:artist).where(artist: { user: current_user })
   end
 end
