df5b0cbec43da0970dd9445a82fcfe4c860e4c5c
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-03-02 15:59:06 +0100

    login logout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
    skip_before_action :authenticate_user!, only: %i[home]
+   before_action :check_signed_in
+
+  def check_signed_in
+    redirect_to artists_path if signed_in?
+  end
+
   def home
     @artists = Artist.all
   end
