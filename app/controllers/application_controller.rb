1fde1716c104b0e0a54039f02501b2660ef5125c
julie thomas <juliethomas1008@gmail.com>
2023-03-03 12:50:20 +0100

    rajout des if dans les notif de la navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :notification_user
+  before_action :notification_manager
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
+  end
+
+  def notification_user
+    array = current_user.bookings.map do |booking|
+      booking.just_validated
+    end
+    @notif_user = array.include?(true)
+  end
+
+  def notification_manager
+    array = []
+    current_user.artists.each do |artist|
+      artist.bookings.each do |booking|
+        array << booking.validated
+      end
+    end
+    @notif_manager = array.include?("pending")
   end
 end
